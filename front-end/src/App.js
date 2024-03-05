db852365ff9df669a83b57af87e2b62601f2bfc8
William_Cui <wc2127@nyu.edu>
2024-03-05 10:03:38 -0500

    added new medicine component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Your App's custom styling
+import MedicationCard from './MedicationCard'; // Import the MedicationCard component
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* You can put other header content here */}
       </header>
+      <main className="App-content">
+        {/* Include the MedicationCard component */}
+        <MedicationCard />
+        <MedicationCard />
+      </main>
     </div>
   );
 }
