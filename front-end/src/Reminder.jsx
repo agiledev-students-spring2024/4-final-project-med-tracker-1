ec3fbc431de8e2bb497ac98e172c55b1a0f087d9
William_Cui <wc2127@nyu.edu>
2024-03-06 17:17:28 -0500

    Merge branch 'master' of https://github.com/agiledev-students-spring2024/4-final-project-med-tracker-1

--- front-end/src/Reminder.jsx
+++ front-end/src/Reminder.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
-const Reminder = () => {
-    return(<></>)
-}
+import './Reminder.css'; // make sure to create a corresponding CSS file
 
-export default Reminder
+const Reminder = () => {
+    return (
+        <div className="reminder-page">
+            <header className="header">
+                <h1>Reminder</h1>
+            </header>
+            <div className="card medication">
+                <h2>Midol</h2>
+                <div className="pill-count">26 pills left</div>
+            </div>
+            <div className="card dose">
+                <h2>Dose</h2>
+                <div>1 pill(s)</div>
+            </div>
+            <div className="card time">
+                <h2>Time</h2>
+                <div>08:00</div>
+            </div>
+            <div className="buttons">
+                <button className="btn confirm">Confirm</button>
+                <button className="btn later">Later</button>
+                <button className="btn skip">Skip</button>
+            </div>
+        </div>
+    );
+};
+
+export default Reminder;
