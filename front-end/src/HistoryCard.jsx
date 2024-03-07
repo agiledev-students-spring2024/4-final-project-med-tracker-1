390a98cdd3a3570bb652ad477296de87f50975b8
TomatoIsGod <sx2113@nyu.edu>
2024-03-06 22:06:25 -0500

    update the history page

---
+++ front-end/src/HistoryCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HistoryCard.css';
+
+const HistoryCard = ({ name, pillsLeft, schedule, date }) => {
+    return (
+        <div className="medication-card">
+            <div className="medication-info">
+                <h1>{name}</h1>
+                <p>{pillsLeft} pill(s) left</p>
+                <p>{schedule}</p>
+                <p>{date}</p>
+            </div>
+            <div className="medication-image">
+                {/* Placeholder for medication image */}
+            </div>
+        </div>
+    );
+};
+
+export default HistoryCard;
