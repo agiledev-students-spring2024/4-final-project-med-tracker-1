36c8b68e985c70c672fcf6a3a3af41cd63c199ed
bettylirf <bettylirf@gmail.com>
2024-03-23 23:31:25 -0400

    changed nav bar

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import {ReactComponent as HomeIcon} from './icons/Home.svg';
+import {ReactComponent as HistoryIcon} from './icons/History.svg';
+import {ReactComponent as MedIcon} from './icons/Pill.svg';
+import {ReactComponent as SettingIcon} from './icons/Setting.svg';
 import './NavBar.css'
 
 const NavBar = props => {
@@ -8,16 +12,36 @@ const NavBar = props => {
             <nav className="nav-bar">
                 <ul className="nav-links">
                     <li className="nav-item">
-                        <Link className="links" to="/home">Home</Link>
+                        <div className="nav-box">
+                        <Link className="links" to="/home">
+                            <HomeIcon className="icon"/>
+                            <p className="nav-text">Home</p>
+                        </Link>
+                        </div>
                     </li>
                     <li className="nav-item">
-                        <Link className="links" to="/history">History</Link>
+                        <div className="nav-box">
+                        <Link className="links" to="/history">
+                            <HistoryIcon className="icon"/>
+                            <p className="nav-text">History</p>
+                        </Link>
+                        </div>
                     </li>
                     <li className="nav-item">
-                        <Link className="links" to="/medicines">Medicines</Link>
+                        <div className="nav-box">
+                        <Link className="links" to="/medicines">
+                            <MedIcon className="icon"/>
+                            <p className="nav-text">Medicines</p>
+                        </Link>
+                        </div>
                     </li>
                     <li className="nav-item">
-                        <Link className="links" to="/setting">Setting</Link>
+                        <div className="nav-box">
+                        <Link className="links" to="/setting">
+                            <SettingIcon className="icon"/>
+                            <p className="nav-text">Setting</p>
+                        </Link>
+                        </div>
                     </li>
                 </ul>
             </nav>
