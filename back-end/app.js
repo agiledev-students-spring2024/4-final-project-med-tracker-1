068bcff5f8689ad3b599fbcf4eb7d0e4b0a4dec4
TomatoIsGod <sx2113@nyu.edu>
2024-03-27 15:59:25 -0400

    updated history page back-end code

--- back-end/app.js
+++ back-end/app.js
@@ -8,9 +8,14 @@ app.use(cors());
 
 app.get('/home', (req, res) => {
     const medications = [
-      { name: 'Midol', pillsLeft: 26, schedule: '8:00PM', date: 'Mar 27th' },
-      { name: 'Vitamin C', pillsLeft: 15, schedule: '6:00PM', date: 'Mar 27th' },
+      { name: 'Midol', pillsLeft: 26, schedule: '6:00PM', date: 'Mar 26th' },
+      { name: 'Midol', pillsLeft: 26, schedule: '7:00PM', date: 'Mar 27th' },
+      { name: 'Midol', pillsLeft: 26, schedule: '8:00PM', date: 'Mar 28th' },
+      { name: 'Vitamin C', pillsLeft: 15, schedule: '9:00PM', date: 'Mar 27th' },
+      { name: 'Vitamin C', pillsLeft: 15, schedule: '11:00PM', date: 'Mar 27th' },
+      { name: 'Zinc', pillsLeft: 10, schedule: '8:00AM', date: 'Feb 12th' },
       { name: 'Zinc', pillsLeft: 10, schedule: '8:00AM', date: 'Apr 12th' },
+      
     ];
   
     const now = new Date();
@@ -50,6 +55,48 @@ app.get('/home', (req, res) => {
 
     res.json(medicationsToTake);
 });
+
+app.get('/history', (req, res) => {
+    const medications = [
+        { name: 'Midol', pillsLeft: 26, schedule: '6:00PM', date: 'Mar 26th' },
+        { name: 'Midol', pillsLeft: 26, schedule: '7:00PM', date: 'Mar 27th' },
+        { name: 'Midol', pillsLeft: 26, schedule: '8:00PM', date: 'Mar 28th' },
+        { name: 'Vitamin C', pillsLeft: 15, schedule: '9:00PM', date: 'Mar 27th' },
+        { name: 'Vitamin C', pillsLeft: 15, schedule: '11:00PM', date: 'Mar 27th' },
+        { name: 'Zinc', pillsLeft: 10, schedule: '8:00AM', date: 'Feb 12th' },
+        { name: 'Zinc', pillsLeft: 10, schedule: '8:00AM', date: 'Apr 12th' },
+    ];
+  
+    const now = new Date();
+    let medicationsTaken = [];
+
+    medications.forEach(medication => {
+      const dateParts = medication.date.match(/(\w+)\s(\d+)[a-z]{2}/);
+      const month = dateParts[1];
+      const day = parseInt(dateParts[2]);
+      const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+      const monthNumber = monthNames.indexOf(month);
+      const medicationDate = new Date(now.getFullYear(), monthNumber, day);
+  
+      if (medicationDate < now || (medicationDate.toDateString() === now.toDateString())) {
+        const scheduleParts = medication.schedule.match(/(\d+):(\d+)(AM|PM)/);
+        let hours = parseInt(scheduleParts[1]);
+        const minutes = parseInt(scheduleParts[2]);
+        const isPM = scheduleParts[3] === 'PM';
+  
+        if (isPM && hours < 12) hours += 12;
+        if (!isPM && hours === 12) hours = 0;
+
+        const medicationDateTime = new Date(now.getFullYear(), monthNumber, day, hours, minutes);
+        if (medicationDateTime <= now) {
+          medicationsTaken.push(medication);
+        }
+      }
+    });
+
+    res.json(medicationsTaken);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
