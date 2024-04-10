d794a044e8cecfb4fdcf005d6b82efdd3307f017
alexh212 <136022829+alexh212@users.noreply.github.com>
2024-04-10 18:46:24 -0400

    Fix Login.jsx

--- front-end/src/Login.jsx
+++ front-end/src/Login.jsx
@@ -10,12 +10,11 @@ const Login = () => {
     const handleLoginSubmit = async (event) => {
         event.preventDefault();
         const formData = {
-            username: document.getElementById('username').value,
+            email: document.getElementById('username').value,
             password: document.getElementById('password').value,
         };
 
         try {
-            console.log('Request to:', `${process.env.REACT_APP_SERVER_HOSTNAME}/api/login`);
             const response = await fetch(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/login`, {
                 method: 'POST',
                 headers: {
@@ -24,15 +23,20 @@ const Login = () => {
                 body: JSON.stringify(formData),
             });
 
-            const data = await response.json();
-
-            if (response.ok) {
-                setIsLoggedIn(true);
-            } else {
-                setLoginError(data.message || 'Login failed. Please check your credentials.');
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
+
+            try {
+                const data = await response.json();
+                setIsLoggedIn(true);
+            } catch (error) {
+                throw new Error("Received non-JSON response from server.");
+            }
+
         } catch (error) {
-            setLoginError('Server error. Please try again later.');
+            console.error('Error logging in:', error);
+            setLoginError(error.message || 'Login failed. Please check your credentials.');
         }
     };
 
@@ -45,15 +49,15 @@ const Login = () => {
             <img className="app-logo" src={logo} alt="app logo"/>
             <h1 className="app-title">Med Tracker</h1>
             <form className="input-form login-form" onSubmit={handleLoginSubmit}>
-                <input className="input-box" type="text" id="username" placeholder="Username" />
+                <input className="input-box" type="text" id="username" placeholder="Email" />
                 <input className="input-box" type="password" id="password" placeholder="Password" />
                 <button type="submit" className="blue-btn">Log in</button>
                 {loginError && <div className="error-message">{loginError}</div>}
             </form>
             <div className="register-container">
                 <p>Don't have an account?</p>
-                <Link to="/register" className="sign-up-btn">Sign up</Link> 
-            </div> 
+                <Link to="/register" className="sign-up-btn">Sign up</Link>
+            </div>
         </div>
     );
 };
